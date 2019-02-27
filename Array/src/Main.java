80fa8edc6fbc119f4627d98c82a03327cc5bb6b1
niujianwei <longshanzhiyu_2018@163.com>
2019-02-27 18:56:50 +0800

    给自定义数组添加查询、修改元素的方法

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -3,16 +3,27 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        int[] arr = new int[20];
-        for (int i = 0; i < arr.length; i++)
-            arr[i] = i;
+//        int[] arr = new int[20];
+//        for (int i = 0; i < arr.length; i++)
+//            arr[i] = i;
+//
+//        int[] scores = new int[]{100,99,66};
+//        for (int i = 0; i< scores.length; i++)
+//            System.out.println(scores[i]);
+//
+//        for (int score: scores) {
+//            System.out.println(score);
+//        }
 
-        int[] scores = new int[]{100,99,66};
-        for (int i = 0; i< scores.length; i++)
-            System.out.println(scores[i]);
+        Array arr = new Array(20);
+        for (int i=0; i< 10; i++)
+            arr.addLast(i);
+        System.out.println(arr);
 
-        for (int score: scores) {
-            System.out.println(score);
-        }
+        arr.add(1, 100);
+        System.out.println(arr);
+
+        arr.addFirst(-1);
+        System.out.println(arr);
     }
 }

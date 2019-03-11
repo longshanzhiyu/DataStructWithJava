12eb8d994ab61bfafd2c74dec166b65653d0939b
niujianwei <longshanzhiyu_2018@163.com>
2019-03-11 14:17:55 +0800

    链表添加元素 使用虚拟头节点 链表的遍历、查询和修改

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -23,4 +23,129 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+//    private Node head;
+    private Node dummyHead; //虚拟头节点
+    private int size;
+
+    public LinkedList(){
+//        head =null;
+        dummyHead = new Node(null, null);
+        size = 0;
+    }
+
+    //获取链表中的元素个数
+    public int getSize() {
+        return size;
+    }
+
+    //返回链表是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    //在链表的index(0-based)位置添加新的元素e
+    //在链表中不是一个常用的操作，联系用：
+    public void add(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+
+        // 此处需要对往0位置添加新元素做额外处理， 可以通过给链表添加虚拟头节点进行解决
+//        if (index == 0)
+//            addFirst(e);
+//        else {
+//            Node prev = head;
+            Node prev = dummyHead;
+//            for (int i=0; i<index-1; i++)
+            for (int i=0; i<index; i++)
+                prev = prev.next;
+
+//            Node node = new Node(e);
+//            node.next = prev.next;
+//            prev.next = node;
+
+            prev.next = new Node(e, prev.next);
+            size ++;
+//        }
+    }
+
+    //在链表头部添加新的元素e
+    public void addFirst(E e) {
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+
+//        head = new Node(e, head);  //此处一句话 完成了 上面三句话的功能
+        add(0, e);
+        size++;
+    }
+
+    //在链表末尾添加新的元素
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    // 获得链表的第index(0-based)个位置的元素
+    // 在链表中不是一个常用的操作，练习用：）
+    public E get(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for (int i=0; i<index; i++)
+            cur = cur.next;
+
+        return cur.e;
+    }
+
+    //获取链表的第一个元素
+    public E getFirst() {
+        return get(0);
+    }
+
+    // 获取链表的最后一个元素
+    public E getLast(){
+        return get(size - 1);
+    }
+
+    // 修改链表的第index(0-based)个位置的元素
+    // 在链表中不是一个常用的操作，练习用：）
+    public void set(int index, E e) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for (int i=0; i<index; i++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    // 查找链表中是否有元素e
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.equals(e))
+                return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+
+//        Node cur = dummyHead.next;
+//        while (cur != null) {
+//            res.append(cur + "->");
+//            cur = cur.next;
+//        }
+
+        for (Node cur = dummyHead.next; cur != null; cur = cur.next)
+            res.append(cur + "->");
+
+        res.append("NULL");
+
+        return res.toString();
+    }
 }
--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        for (int i=0; i<5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
     }
 }

9bda7b5c04f23ff6bd1e5ad4cfb50f451b1802e1
niujianwei <longshanzhiyu_2018@163.com>
2019-03-15 13:15:55 +0800

    用打印的方式 调试递归

--- remove-linked-list-element/src/Solution3.java
+++ remove-linked-list-element/src/Solution3.java
@@ -1,15 +1,41 @@
+import java.util.List;
+
 public class Solution3 {
-    public ListNode removeElements(ListNode head, int val) {
+    public ListNode removeElements(ListNode head, int val, int depth) {
 
-        if (head == null)
-            return null;
+        String depthString = generateDepthString(depth);
 
-        head.next = removeElements(head.next, val);
-        if (head.val == val)
-            return head.next;
-        else
+        System.out.print(depthString);
+        System.out.println("Call: remove " + val + " in " + head);
+
+        if (head == null) {
+            System.out.print(depthString);
+            System.out.println("Return: " + head);
             return head;
-//        return head.val == val ? head.next : head;
+        }
+
+        ListNode res = removeElements(head.next, val, depth+1);
+        System.out.print(depthString);
+        System.out.println("After remove " + val + ": " + res);
+
+        ListNode ret;
+        if (head.val == val)
+            ret = res;
+        else {
+            head.next = res;
+            ret = head;
+        }
+
+        System.out.print(depthString);
+        System.out.println("Return " + ret);
+        return ret;
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i=0; i<depth; i++)
+            res.append("--");
+        return res.toString();
     }
 
     public static void main(String[] args) {
@@ -17,7 +43,7 @@ public class Solution3 {
         ListNode head = new ListNode(nums);
         System.out.println(head);
 
-        ListNode res = (new Solution3()).removeElements(head, 6);
+        ListNode res = (new Solution3()).removeElements(head, 6, 0);
         System.out.println(res);
     }
 }

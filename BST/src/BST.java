5c68731880655f23970ec1909d06dd4c29e325d5
niujianwei <longshanzhiyu_2018@163.com>
2019-03-18 10:43:54 +0800

    改进 添加元素的方法

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -29,33 +29,24 @@ public class BST<E extends Comparable<E>> {
 
     // 向二分搜索树中添加新的元素e
     public void add(E e) {
-        if (root == null) {
-            root = new Node(e);
-            size ++;
-        }
-        else
-            add(root, e);
+        root = add(root, e);
     }
 
     //  向以弄的为根的二分搜索树中插入元素E，递归算法
-    private void add(Node node, E e) {
-        if (e.equals(node.e))
-            return;
-        else if (e.compareTo(node.e) < 0 && node.left.e == null) {
-            node.left = new Node(e);
-            size++;
-            return;
-        }
-        else if (e.compareTo(node.e) > 0 && node.right.e == null) {
-            node.right = new Node(e);
-            size++;
-            return;
+    //  返回插入新节点后二分搜索树的根
+    private Node add(Node node, E e) {
+
+        if (node == null) {
+            size ++;
+            return new Node(e);
         }
 
         if (e.compareTo(node.e) < 0)
-            add(node.left, e);
-        else
-            add(node.right, e);
+            node.left = add(node.left, e);
+        else if (e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
     }
 
 }

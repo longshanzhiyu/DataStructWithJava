2f48a8fc4e15742b98559c63e7d1bcd33083d344
niujianwei <longshanzhiyu_2018@163.com>
2019-03-19 11:19:24 +0800

    二分搜索树 非递归前序遍历

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class BST<E extends Comparable<E>> {
 
     private class Node {
@@ -85,6 +87,21 @@ public class BST<E extends Comparable<E>> {
         preOrder(node.left);
         preOrder(node.right);
 //    }
+    }
+
+    // 二分搜索树的非递归前序遍历
+    public void preOrderNR(){
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+
+            if (cur.right != null)
+                stack.push(cur.right);
+            if (cur.left != null)
+                stack.push(cur.left);
+        }
     }
 
     // 二分搜索树的中序遍历
--- BST/src/Main.java
+++ BST/src/Main.java
@@ -11,11 +11,14 @@ public class Main {
         bst.preOrder();
         System.out.println();
 
-        bst.inOrder();
+        bst.preOrderNR();
         System.out.println();
 
-        bst.postOrder();
-        System.out.println();
+//        bst.inOrder();
+//        System.out.println();
+//
+//        bst.postOrder();
+//        System.out.println();
 
 //        System.out.println(bst);
     }
